a1a4d7315014a5ec06766de08a20dbb504a20aae
wenstarn <34746081+wenstarn@users.noreply.github.com>
2021-09-20 23:03:44 +0300

    Add files via upload

---
+++ yelpCamp/app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const { campgroundSchema, reviewSchema } = require('./schemas')
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'thisissecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        //expires: Date.now(),
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next()
+})
+
+app.use("/campgrounds", campgrounds)
+app.use("/campgrounds/:id/reviews", reviews)
+
+app.get('/', (req, res) => {
+
+    res.render('home');
+})
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { status = 500 } = err;
+    if (!err.message) err.message = 'Something wrong'
+    res.status(status).render('error', { err });
+})
+
+app.listen(3000, () => {
+    console.log("Listen on 3000 port !!!")
+})
+
